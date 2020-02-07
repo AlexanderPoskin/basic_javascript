733a248cf14dc039f5ddea8b4c6a2c7409b93a4a
AlexanderPoskin <sumstor11@gmail.com>
2020-02-07 12:44:15 +0200

    Adds 8 task

--- basic_javascript/8/8.js
+++ basic_javascript/8/8.js
@@ -1,11 +1,11 @@
 function rentalCarCost(n, cost = 40) {
-  if (n < 3) {
-    return n * cost;
-  } else if (n > 3 && n < 7) {
-    return n * cost - 20;
-  } else if (n >= 7) {
-    return n * cost - 50;
+    if (n < 3) {
+      return n * cost;
+    } else if (n > 3 && n < 7) {
+      return n * cost - 20;
+    } else if (n >= 7) {
+      return n * cost - 50;
+    }
   }
-}
-
-console.log(rentalCarCost(2));
+  
+  console.log(rentalCarCost(2));

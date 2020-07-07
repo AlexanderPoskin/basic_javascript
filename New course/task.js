c55aec8343619a25d54aad864f31cab9731573ec
AlexanderPoskin <sumstor11@gmail.com>
2020-07-07 16:28:56 +0300

    !

--- New course/task.js
+++ New course/task.js
@@ -22,7 +22,6 @@ function rememberMyFilms() {
   for (let i = 0; i < 2; i++) {
     const a = prompt("Один из последних просмотренных фильмов?", ""),
       b = prompt("На сколько оцените его?", "");
-
     if (a != null && b != null && a != "" && b != "" && a.length < 50) {
       personalMovieDB.movies[a] = b;
       console.log("done");
